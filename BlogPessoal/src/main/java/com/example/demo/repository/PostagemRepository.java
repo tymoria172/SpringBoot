941aedd8dde092bb4d700aefb4c61be7b44df9ff
Proevento <andre.ricardo2115@gmail.com>
2021-09-27 13:50:49 -0300

    Adicionando metodos

--- BlogPessoal/src/main/java/com/example/demo/repository/PostagemRepository.java
+++ BlogPessoal/src/main/java/com/example/demo/repository/PostagemRepository.java
@@ -1,5 +1,7 @@
 package com.example.demo.repository;
 
+import java.util.List;
+
 //import java.util.List;
 
 import org.springframework.data.jpa.repository.JpaRepository;
@@ -7,9 +9,22 @@ import org.springframework.stereotype.Repository;
 
 import com.example.demo.model.Postagem;
 
+/**
+ * 
+ * A annotation @Repository indica que a Classe é uma Repository, ou seja, é responsável 
+ * pela comunicação com o Banco de dados através dos métodos padrão e das Method Queries, 
+ * que são consultas personalizadas através de palavras chave que representam as instruções 
+ * da linguagem SQL
+ * 
+ */
+
 @Repository
 public interface PostagemRepository extends JpaRepository<Postagem, Long> {
 
-	//public List<Postagem> finAllbyTituloContainingIgnoreCase(String titulo);
+	/**
+	 * Esta method Query é equivalente a: select * from tb_postagem where titulo like "%titulo%";
+	 */
+	
+	public List <Postagem> findAllByTituloContainingIgnoreCase(String titulo);
 
 }
