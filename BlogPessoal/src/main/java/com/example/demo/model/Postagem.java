39e194d233d7c071f2d40adc3591ba474a56c9a9
Proevento <andre.ricardo2115@gmail.com>
2021-10-06 15:18:52 -0300

    Adicionando Security

--- BlogPessoal/src/main/java/com/example/demo/model/Postagem.java
+++ BlogPessoal/src/main/java/com/example/demo/model/Postagem.java
@@ -15,56 +15,79 @@ import javax.validation.constraints.Size;
 
 import com.fasterxml.jackson.annotation.JsonIgnoreProperties;
 
-@Entity //gera tabela no mysql
-@Table(name = "tb_postagem") // definir nome da tabela 
+@Entity // gera tabela no mysql
+@Table(name = "tb_postagem") // definir nome da tabela
 public class Postagem {
 
-	@Id //PK
+	@Id // PK
 	@GeneratedValue(strategy = GenerationType.IDENTITY) // AUTO INCREMENTO DA CHAVE PRIMARIA
 	private long id;
+
 	@NotNull // NOT NULL
 	@Size(min = 5, max = 100) // DEFINE O TAMANHO
 	private String titulo;
+
 	@NotNull
 	@Size(min = 10, max = 1000, message = " Texto deve conter no minimo 10 digitos a 1000 digitos")
 	private String texto;
-	@Temporal(TemporalType.TIMESTAMP) //Define o tempo atravez do times tamp
-	private Date data = new java.sql.Date(System.currentTimeMillis()); //Define biblioteca e pega os milisegundos
+
+	@Temporal(TemporalType.TIMESTAMP) // Define o tempo atravez do times tamp
+	private Date data = new java.sql.Date(System.currentTimeMillis());// Define biblioteca e pega os milisegundos
+
 	@ManyToOne
 	@JsonIgnoreProperties("postagem")
 	private Tema tema;
-	
-	
+
+	@ManyToOne
+	@JsonIgnoreProperties("postagem")
+	private Usuario usuario;
+
+	public Usuario getUsuario() {
+		return usuario;
+	}
+
+	public void setUsuario(Usuario usuario) {
+		this.usuario = usuario;
+	}
+
 	public Tema getTema() {
 		return tema;
 	}
+
 	public void setTema(Tema tema) {
 		this.tema = tema;
 	}
+
 	public long getId() {
 		return id;
 	}
+
 	public void setId(long id) {
 		this.id = id;
 	}
+
 	public String getTitulo() {
 		return titulo;
 	}
+
 	public void setTitulo(String titulo) {
 		this.titulo = titulo;
 	}
+
 	public String getTexto() {
 		return texto;
 	}
+
 	public void setTexto(String texto) {
 		this.texto = texto;
 	}
+
 	public Date getData() {
 		return data;
 	}
+
 	public void setData(Date data) {
 		this.data = data;
 	}
 
-	
 }
